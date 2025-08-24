e6893219a91afa8b5a960ddbfd2e527c7cc74360
Abrar Agmain Shazid <abraragmain75@gmail.com>
2025-08-25 01:46:43 +0600

    .

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -2,7 +2,7 @@ import mongodb from '@/lib/mongodb';
 import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
-
+export const dynamic = "force-dynamic"; export const revalidate = 0;
 
 export const metadata = {
   title: "NexBuy | All Products",
